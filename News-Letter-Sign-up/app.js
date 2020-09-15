03cc1db579e8e399f75a6242ef506956df42965c
kapurm17 <kapurm17@gmail.com>
2020-09-15 21:01:29 +0530

    NewsLetter-Signup

---
+++ News-Letter-Sign-up/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const requests = require("request");
+
+const app = express();
+
+// express.static to add static file dependencies in the html page i.e styles.css here, images
+// argument is the folder where all thestatic files are kept and html linking is done relative
+// to this folder and not the current dir
+app.use(express.static("styles"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  let fname = req.body.fname;
+  let lname = req.body.lname;
+  let email = req.body.email;
+
+  console.log(fname + lname + email);
+});
+
+app.listen(3000, ()=> {
+  console.log("Server set up at port 3000");
+});
